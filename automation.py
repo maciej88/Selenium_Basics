17692c27b5cff555cb4ec4573bce019fff85bdb4
maciej <mkrywult88@gmail.com>
2022-09-22 18:54:30 +0200

    message input - no aprove

--- automation.py
+++ automation.py
@@ -13,3 +13,7 @@ chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
 
 button_text = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
 print(button_text.get_attribute('innerHTML'))
+
+user_message = chrome_browser.find_element(By.ID, 'user-message')
+user_message.clear()
+user_message.send_keys('Light weight!')
