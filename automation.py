1c751a7d79ab80222af28fc1e41d0abecb8cbe97
maciej <mkrywult88@gmail.com>
2022-09-22 18:50:47 +0200

    button grab

--- automation.py
+++ automation.py
@@ -1,8 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
 
 service = Service(executable_path='./chromedriver')
 
 chrome_browser = webdriver.Chrome(service=service)
 
 chrome_browser.maximize_window()
+
+chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+
+button_text = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
+print(button_text.get_attribute('innerHTML'))
