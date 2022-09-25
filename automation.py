84513a059eb4d327faf2cb93d69f3ceaa8f35b13
maciej <mkrywult88@gmail.com>
2022-09-25 15:58:03 +0200

    add assertion - is this correct site

--- automation.py
+++ automation.py
@@ -9,9 +9,15 @@ chrome_browser = webdriver.Chrome(service=service)
 chrome_browser.maximize_window()
 
 chrome_browser.get('https://demo.seleniumeasy.com/basic-first-form-demo.html')
+
 assert 'Selenium Easy Demo' in chrome_browser.title
+
 button_add = chrome_browser.find_element(By.CLASS_NAME, 'btn-default')
+
+assert 'Show Message' in chrome_browser.page_source
+
 print(button_add.get_attribute('innerHTML'))
+
 
 user_message = chrome_browser.find_element(By.ID, 'user-message')
 user_message.clear()
